d7d085680b0515f90d04216667fe080de2bb851a
Intrepidpay <emekabright737@gmail.com>
2025-09-04 22:54:57 +0000

    Save local changes before rebase

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',   // 👈 Important: root base for custom domains
   server: {
     port: 3000,
-    open: true
+    open: true,
   },
-  base: '/multi-cap/'
 })
