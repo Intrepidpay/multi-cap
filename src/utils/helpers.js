0aa921230d8cb7e5ae31c842bf72dfcbba4371ef
Intrepidpay <emekabright737@gmail.com>
2025-09-12 00:09:06 +0100

    Update helpers.js

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,8 +1,11 @@
 export const formatPrice = (price) => {
   return new Intl.NumberFormat('en-US', {
     style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2
-  }).format(price);
-};
+    currency: 'USD'
+  }).format(price)
+}
+
+export const truncateText = (text, maxLength) => {
+  if (text.length <= maxLength) return text
+  return text.substring(0, maxLength) + '...'
+}
