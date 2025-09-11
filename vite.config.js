8ce24ab03dd8584d8a27e02f534eb4c093669a62
Intrepidpay <emekabright737@gmail.com>
2025-09-12 00:28:00 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import sitemap from 'vite-plugin-sitemap' // Use default import instead of named import
+import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
   plugins: [
     react(),
     sitemap({
-      baseUrl: 'https://www.multicapital.online',
-      outDir: 'dist',
+      hostname: 'https://www.multicapital.online',
+      outDir: 'dist', 
+      generateRobotsTxt: false,
     }),
   ],
-  base: '/',   // Important: root base for custom domains
+  base: '/',   
   server: {
     port: 3000,
     open: true,
