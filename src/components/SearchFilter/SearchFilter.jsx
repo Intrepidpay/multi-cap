0b3db768b19034e3931c03e389a84b817242ec81
Intrepidpay <emekabright737@gmail.com>
2025-09-14 18:16:36 +0100

    Update SearchFilter.jsx

--- src/components/SearchFilter/SearchFilter.jsx
+++ src/components/SearchFilter/SearchFilter.jsx
@@ -3,7 +3,7 @@ import './SearchFilter.css'
 const SearchFilter = ({ filter, setFilter, sort, setSort, searchQuery, setSearchQuery }) => {
   const categories = [
     'all', 'trucks', 'cars', 'motorcycles', 'bicycles', 'tv', 
-  'home appliances', 'laptops', 'desktops', 'game consoles', 'collectable' , 'art' , 'fashion' , 'accessories', 'rare'  , 'other'
+  'home appliances', 'laptops', 'desktops', cameras, 'game consoles', 'collectable' , 'art' , 'fashion' , 'accessories', 'rare'  , 'other'
 ]
 
   return (
